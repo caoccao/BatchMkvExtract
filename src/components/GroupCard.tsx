fec0fd6eeac472370c86e998af4db879d1debc25
Sam Cao <sjtucaocao@gmail.com>
2026-04-19 16:03:56 +0200

    fix: remove minHeight constraint from GroupCard component's split container

--- src/components/GroupCard.tsx
+++ src/components/GroupCard.tsx
@@ -419,7 +419,7 @@ export function GroupCard({ files }: GroupCardProps) {
       </Box>
       <Box
         ref={splitContainerRef}
-        sx={{ display: "flex", minHeight: 200, position: "relative" }}
+        sx={{ display: "flex", position: "relative" }}
       >
         <Box
           sx={{
